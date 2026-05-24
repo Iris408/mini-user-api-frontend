631c2192124a9f0b09230c11cdc87960aa0f956e
Ashleigh Magloire <amagloire@Ashleighs-MacBook-Pro.local>
2026-05-24 20:38:11 +0100

    Fix React import build issue

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import React from "react";
 import "./styles.css";
 import App from './App.tsx'
 
